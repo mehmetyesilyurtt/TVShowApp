13ea2021a9f6d6dcd4bb9702dd9525affc6de6e0
Mehmet Yeşilyurt <yesilyurtt.mehmett@gmail.com>
2022-08-22 00:39:58 +0300

    Initialized new project

---
+++ TVShowApp/TVShowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVShowApp
+//
+//  Created by Gürkan Yeşilyurt on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
